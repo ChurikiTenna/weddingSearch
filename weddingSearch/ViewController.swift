2d3110d83b75ff422d137146be58728e09ff4e33
ChurikiTenna <52132649+ChurikiTenna@users.noreply.github.com>
2022-02-02 11:54:00 +0900

    Initial Commit

---
+++ weddingSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weddingSearch
+//
+//  Created by 中力天和 on 2022/02/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
